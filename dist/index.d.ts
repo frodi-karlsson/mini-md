64ea061190b58a2856ab6184f6916ed128b31482
Frodi Karlsson <frodikarlsson@gmail.com>
2023-10-05 12:15:47 +0200

    change types to dist/ again?

---
+++ dist/index.d.ts
@@ -0,0 +1,8 @@
+declare namespace _default {
+    export { MiniMD };
+    export { Template };
+}
+export default _default;
+import MiniMD from "./src/MiniMd.js";
+import Template from "./src/Template.js";
+export { MiniMD, Template };
