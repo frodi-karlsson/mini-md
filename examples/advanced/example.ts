552f173831508321a3abfde5d5293bfb0cd60d91
Fróði Karlsson <frodikarlsson@gmail.com>
2024-04-13 02:16:31 +0200

    refactor: clean up and optimize

--- examples/advanced/example.ts
+++ examples/advanced/example.ts
@@ -1,13 +1,12 @@
 import express from "express";
-import path from "path";
 import { miniMd } from "mini-md";
 import markdownItAttrs from "markdown-it-attrs";
 
 const app = express();
-app.use(express.static(path.resolve(__dirname, "public")));
+app.use(express.static('public'))
 app.use(
   miniMd({
-    rootDir: path.resolve(__dirname, "md"),
+    rootDir: "md",
     mdOptions: {
         html: true,
     },
--- examples/basic/example.ts
+++ examples/basic/example.ts
@@ -1,11 +1,10 @@
 import express from "express";
-import path from "path";
 import { miniMd } from "mini-md";
 
 const app = express();
 app.use(
   miniMd({
-    rootDir: path.resolve(__dirname, "md"),
+    rootDir: "md",
   })
 );
 
